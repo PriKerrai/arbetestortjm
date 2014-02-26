901d80a25fca2025fe1062d3c86176d458febbc8
magnusson1987@gmail.com <magnusson1987@gmail.com@f028673f-e49a-703d-21bf-89727852b0bf>
2014-02-26 08:22:57 +0000

    CSS

--- ComaExjobb/src/com/coma/comaexjobb/client/ComaExjobb.java
+++ ComaExjobb/src/com/coma/comaexjobb/client/ComaExjobb.java
@@ -47,12 +47,14 @@ public class ComaExjobb implements EntryPoint {
 		// We can add style names to widgets
 		sendButton.addStyleName("sendButton");
 
+		/*
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
 		RootPanel.get("nameFieldContainer").add(nameField);
 		RootPanel.get("sendButtonContainer").add(sendButton);
 		RootPanel.get("errorLabelContainer").add(errorLabel);
-
+		 */
+		
 		// Focus the cursor on the name field when the app loads
 		nameField.setFocus(true);
 		nameField.selectAll();
